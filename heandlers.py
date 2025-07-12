5e7cf3ea4fd96a09e60351f10223b693ad518aab
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-12 16:40:15 +0300

    фича: добавил просмотр списка заметок

--- heandlers.py
+++ heandlers.py
@@ -1,8 +1,8 @@
 from aiogram.types import Message
 from aiogram.filters import Command
-from db.models import Note
+from db.models import Note, User
 from db.session import SessionLocal
-
+from sqlalchemy import select
 
 async def start_handler(msg: Message):
     """ 
@@ -31,6 +31,12 @@ async def add_handler(msg: Message):
         await msg.answer("Ошибочка, не удалось определить пользователя.")
         return
     async with SessionLocal() as session:
+        result = await session.execute(select(User).where(User.id == user_id))
+        user = result.scalar_one_or_none()
+        if not user:
+            user = User(id=user_id)
+            session.add(user)
+            await session.flush()
         note = Note(user_id=user_id, text=note_text)
         session.add(note)
         await session.commit()
@@ -44,6 +50,21 @@ async def list_handler(msg: Message):
     Args:
         msg(Message): Список добавленных пользователем заметок.
     """
-    await msg.answer("Твои записи.")
+    user_id = msg.from_user.id if msg.from_user and msg.from_user.id else None
+    if not user_id:
+        await msg.answer("Ошибочка, не удалось определить пользователя.")
+        return
+    async with SessionLocal() as session:
+        result = await session.execute(
+            select(Note).where(Note.user_id == user_id).order_by(Note.created_at.desc())
+        )
+        notes = result.scalars().all()
+        if notes:
+            text = "\n\n".join(
+                f"{i+1}. {note.text}\n({note.formatted_time()})"
+                for i, note in enumerate(notes)
+            )
+            await msg.answer(f"Твои записи:\n\n{text}")
+        
     
     
